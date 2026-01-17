b673f2bfa93772112fa7653d089bc845c02c4d96
Tolan676767 <alexshashin.2023@gmail.com>
2026-01-17 09:56:54 +0500

    Правки.Kanyukov

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,14 +11,17 @@ using System.Windows.Shapes;
 
 namespace pr8Kanyukov
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+   
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void UpdateButtonClick(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
