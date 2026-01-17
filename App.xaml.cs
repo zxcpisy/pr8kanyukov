2ed25c3574a16a8f5225caacd12161701b7586b5
alexeys6969 <alexshashin.2023@gmail.com>
2026-01-17 09:11:19 +0500

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace pr8Kanyukov
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
